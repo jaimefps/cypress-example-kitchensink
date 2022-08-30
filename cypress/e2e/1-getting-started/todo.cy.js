6ce42a46d07f94f3537c6e3f154e572cd6c248fc
Jaime Pericas <jaime@cypress.io>
2022-08-30 13:52:44 -0700

    always pass

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 describe("example test", () => {
   it("might pass", () => {
-    let isTails = true
-    isTails = Math.random() > 0.5;
+    let isTails = false
+    // isTails = Math.random() > 0.5;
 
     if (isTails) {
       throw new Error("Intentional Error");
